15c2df67c7344febe78869643eb8e3c4c325a9d6
Martin Otten <martin.otten@innoq.com>
2026-04-01 11:35:16 +0000

    Add safe navigation operator

--- lib/henitai/operators/safe_navigation.rb
+++ lib/henitai/operators/safe_navigation.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
+require "parser/current"
+
 module Henitai
   module Operators
+    # Removes the nil guard from safe navigation calls.
     class SafeNavigation < Henitai::Operator
+      NODE_TYPES = [:csend].freeze
+
+      def self.node_types
+        NODE_TYPES
+      end
+
+      def mutate(node, subject:)
+        return [] unless node.type == :csend
+
+        [
+          build_mutant(
+            subject:,
+            original_node: node,
+            mutated_node: mutated_node(node),
+            description: "removed nil guard"
+          )
+        ]
+      end
+
+      private
+
+      def mutated_node(node)
+        receiver, method_name, *arguments = node.children
+        Parser::AST::Node.new(:send, [receiver, method_name, *arguments])
+      end
     end
   end
 end
---
+++ spec/henitai/operators/safe_navigation_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Henitai::Operators::SafeNavigation do
+  def parse(source)
+    Henitai::SourceParser.parse(source)
+  end
+
+  def mutation_subject
+    Henitai::Subject.new(namespace: "Example", method_name: "safe_navigation")
+  end
+
+  def mutate(source)
+    node = find_nodes(parse(source), :csend).first
+
+    described_class.new.mutate(node, subject: mutation_subject).first
+  end
+
+  it "declares the safe navigation node type" do
+    expect(described_class.node_types).to eq([:csend])
+  end
+
+  it "removes nil guards from safe navigation calls" do
+    mutant = mutate("user&.name")
+
+    expect(mutant).to have_attributes(
+      description: "removed nil guard",
+      mutated_node: satisfy { |node| node.type == :send }
+    )
+  end
+
+  it "preserves safe navigation arguments when removing the guard" do
+    mutant = mutate("user&.name(1)")
+
+    expect(mutant.mutated_node.children[2].type).to eq(:int)
+  end
+
+  it "ignores regular sends" do
+    node = find_nodes(parse("user.name"), :send).first
+
+    expect(described_class.new.mutate(node, subject: mutation_subject)).to eq([])
+  end
+end
